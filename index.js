faca8c14071b4099156ee8c196a3dafc596e5dd0
Nathan Lewan <nathanlewan@gmail.com>
2022-04-19 16:52:27 -0400

    updates

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-const server = require('./lib/base/index')
+const path = require( 'path' );
+const server = require('./lib/base/index');
+const servers = server("lib/base");
 
-server("lib/base")
+
+const routes = {
+    "testing_route": require ( path.join(__dirname, 'lib', 'routes', 'testing_route', 'testing_route-routes') )
+}
+
+
+
+
+// HTTP or HTTPS Routes
+if (servers.expressApp != false) {
+
+    servers.expressApp.use( '/testing', routes.testing_route.WebRouter(servers.baseConfig) )
+
+}
+
+
+// Socket-io Routes
+if ( servers.socketioApp != false ) { servers.socketioApp.on('connection', (socket) => {
+
+    socket.on('/testing', (data) => { routes.testing_route.IoRouter(socket, data, servers.baseConfig) });
+
+})}
