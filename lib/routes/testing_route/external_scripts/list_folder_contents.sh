1eb94ad4d5f0121bbe836aae87f5af4212f37ad4
Nathan Lewan <nathanlewan@gmail.com>
2022-04-19 20:52:13 -0400

    update testing route

--- lib/routes/testing_route/external_scripts/list_folder_contents.sh
+++ lib/routes/testing_route/external_scripts/list_folder_contents.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-data=$(find $1 -maxdepth 1 -exec stat -c '%F : %n : %U : %G : %s : %w : %y' {} + | sed -e 's|^\.\/||' | sort)
+data=$(find "$1" -maxdepth 1 -exec stat -c '%F : %n : %U : %G : %s : %w : %y' {} + | sed -e 's|^\.\/||' | sort)
 echo "$data"
 
 exit
